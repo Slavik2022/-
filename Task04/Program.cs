249352a8ed8bf2e5ba5b9e919b46992c499f40f6
Slavik <soldatslavik@gmail.com>
2022-10-16 17:20:45 +0300

    Add Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -13,9 +13,9 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 
 int max = number1;
 
-if(number1 > max) max = number1;
-if(number2 > max) max = number2;
-if(number3 > max) max = number3;
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
 
 Console.Write("max = ");
 Console.WriteLine(max);
