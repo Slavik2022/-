ac30d9a45dff125eff40585ed4758d3df3b373f0
Slavik <soldatslavik@gmail.com>
2022-10-17 13:19:22 +0300

    Add Task08, 09, 11, 12, 14, 16

---
+++ Task11/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая 
+// 1. выводит случайное
+// 2. трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число от 100 до 999: {number}");
+// int firstDigit = number / 100;
+// Console.WriteLine($"1 цифра: {firstDigit}");
+// //int secondDigit = number / 10;
+
+// int fithDigit = number % 10;
+// Console.WriteLine($"3 цифра: {fithDigit}");
+
+// int result = firstDigit * 10 + fithDigit;
+// Console.WriteLine($"вывод: {result}");
+
+int RmSecondNum (int number)
+{
+    int FirstDig = number / 100;
+    int ThirdhDig = number % 10;
+    int num1_3 = FirstDig * 10 + ThirdhDig;
+    Console.WriteLine(number);
+    return num1_3;
+}
+
+int res = RmSecondNum(new Random().Next(100, 1000));
+Console.WriteLine(res);
+
---
+++ Task12/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая 
+// 1. будет принимать на вход два числа и
+// 2. выводить, является ли 1 число
+// кратным 2. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = num1 % num2;
+// if (result > 0)
+// {
+//     Console.WriteLine($"Некратное. Остаток от деления: {result}");
+// }
+// else
+// {
+//     Console.WriteLine($"Числа кратны.");
+// }
+
+
+
+
+if (num1 % num2 == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Некратно, остаток {num1 % num2}");
