9d93872f739dbfab047a0595f093f812eb460e3d
Slavik <soldatslavik@gmail.com>
2022-10-21 19:36:53 +0300

    Add Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,16 +1,22 @@
 ﻿// Напишите программу, которая
-// принимает на вход число (N) и выдаёт таблицу
-// квадратов чисел от 1 до N.
+// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
 
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-void SquareTable(int num)
+void CubeNumber(int num)
 {
     if (num > 0)
     {
         for (int i = 1; i <= num; i++)
         {
-            Console.WriteLine($"{i,4}  {i * i,4}");
+            Console.WriteLine($"|{i , 4} | {i * i * i , 4}|");
         }
     }
     else
@@ -18,4 +24,4 @@ void SquareTable(int num)
         Console.WriteLine("Введите число больше 0");
     }
 }
-SquareTable(n);
+CubeNumber(n);
