9ec1b658c0f8dd770b65e60e522cf7618f1afcaa
Slavik <soldatslavik@gmail.com>
2022-10-21 19:03:02 +0300

    Add Task 17,18,20,22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void SquareTable(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i}  {i * i}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите число больше 0");
+    }
+}
+SquareTable(n);
