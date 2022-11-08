bfba388839eaafaf62aa72b842e4120a4750f21f
Slavik <soldatslavik@gmail.com>
2022-11-08 19:59:51 +0300

    Fix Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -9,9 +9,10 @@ int num = Convert.ToInt32(Console.ReadLine());
 int [] NameArray(int size)
 {
     int [] array = new int [size];
+    Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, 2);
+        array[i] = rnd.Next(0, 2);
     }
     return array;
 }
