f8d0a4837435a39adff795d0bda84a5b3c807836
Slavik <soldatslavik@gmail.com>
2022-11-16 21:56:51 +0300

    Add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами.
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    // int size = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int EvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+
+}
+
+int[] arr = CreateArrayRndInt(11, 100, 999);
+PrintArray(arr);
+int result = EvenNumber(arr);
+Console.WriteLine($"Количество четных чисел в массиве, из отрезка [100,999] = {result}");
