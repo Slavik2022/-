411a70431cb30263a4a5eca61aefc881c8379e71
Slavik <soldatslavik@gmail.com>
2022-12-07 18:31:20 +0300

    Add Task64

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -24,3 +24,32 @@ int SumElementNumber(int num)
 
 int sumElemNum = SumElementNumber(number);
 Console.WriteLine($"Сумма его цифр -> {sumElemNum}");
+
+
+// int InputNumber(string message)
+// {
+// while (true)
+// {
+// Console.Write(message);
+// bool result = int.TryParse(Console.ReadLine(), out int value);
+
+// if (!result)
+// {
+// Console.WriteLine($"Введены некоректные данные. {message} еще раз!");
+// Thread.Sleep(1500);
+// Console.Clear();
+
+// continue;
+// }
+
+// if (value < 0)
+// {
+// Console.WriteLine($"Число не может быть отрицательным. {message} еще раз!");
+// Thread.Sleep(1500);
+// Console.Clear();
+
+// continue;
+// }
+// return value;
+// }
+// }
